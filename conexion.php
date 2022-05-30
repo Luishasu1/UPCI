4b8e1ed7673df7b53b060a335a98da9ea8a9ee47
Luishasu1 <105171783+Luishasu1@users.noreply.github.com>
2022-05-30 15:10:24 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,12 @@
+# UPCI<?php
+
+$host = "localhost";
+$usuario = "id18707182_loginupci";
+$contrasena = "pw@_Re|hi_3>Aq-v";
+$bd = "id18707182_login	";
+
+$conexion = mysqli_connect($host,$usuario,$contrasena,$bd);
+
+
+
+?>
