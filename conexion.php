58e8a70f8379f6e68d7f15bffd16e16faf083cee
Luishasu1 <105171783+Luishasu1@users.noreply.github.com>
2022-06-13 12:20:47 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,16 @@
 # UPCI<?php
-
+<?php
+$conn = new mysqli("localhost","id18707182_loginupci","pw@_Re|hi_3>Aq-v","id18707182_login");
+	
+	if($conn->connect_errno)
+	{
+		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
+	}
 $host = "localhost";
 $usuario = "id18707182_loginupci";
 $contrasena = "pw@_Re|hi_3>Aq-v";
-$bd = "id18707182_login	";
+$bd = "id18707182_login";
 
 $conexion = mysqli_connect($host,$usuario,$contrasena,$bd);
-
-
 
 ?>
