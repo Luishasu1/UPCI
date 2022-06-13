1de5de222647483946417c3bea125bb9eaf94769
Luishasu1 <105171783+Luishasu1@users.noreply.github.com>
2022-06-13 12:18:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 # UPCI<!DOCTYPE HTML>
+<!DOCTYPE HTML>
 <title> Login </title>
 <head>
 	<link rel="stylesheet" type="text/css" href="estiloslog.css">
@@ -6,11 +7,20 @@
 <body>
 	<section id="from-login">
       <h4><font size="6">Inicio de Sesión</font></h4>
+      <br></br>
       <form action="login.php" method="POST">
       <input id="cajas_num1" type="text" name="usuario" placeholder="Usuario">
       <input id="cajas_num2" type="password" name="contrasena" placeholder="Contrasena">
     <center>
-      <h5> <p> <a href="NuevaContrasena.html">¿Olvidaste tu contraseña?</a> </p> </h5>
+        <div class="ub1">
+ </div>
+<div class="ub1">Rol</div>
+<select name="rol">
+<option value="0" style="display:none;"><label>Seleccionar</label></option>
+<option value="Usuario">Usuario</option>
+<option value="Admin">Administrador</option>
+</select>
+      <h5> <p> <a href="NuevaContrasena.php">¿Olvidaste tu contraseña?</a> </p> </h5>
     </center>
       <button id="cajas_num3" type="submit">Ingresar</button>
 
